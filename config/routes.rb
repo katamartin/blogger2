d36c1887f14372fc9d34f1cae28004e6fa782d0a
katamartin <katasm@gmail.com>
2015-06-16 20:02:18 -0700

    finished blog comment feature

--- config/routes.rb
+++ config/routes.rb
@@ -54,5 +54,7 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
   root to: 'articles#index'
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 end
